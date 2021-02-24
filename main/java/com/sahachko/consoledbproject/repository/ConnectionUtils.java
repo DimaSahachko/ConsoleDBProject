fc904d94ad66a81480e2616eb7a0df3d17e97b17
Dima Sahachko <sahachko.dima@gmail.com>
2021-02-24 15:41:48 +0200

    File liquibase.properties was added. Statements in ConnectionUtils class which created database and tables in database were deleted.

--- main/java/com/sahachko/consoledbproject/repository/ConnectionUtils.java
+++ main/java/com/sahachko/consoledbproject/repository/ConnectionUtils.java
@@ -75,37 +75,9 @@ public class ConnectionUtils {
 				user = properties.getProperty("user");
 				password = properties.getProperty("password");
 				connection = DriverManager.getConnection(url, user, password);
-				Statement statement = connection.createStatement();
-				statement.execute(DDLqueries.CREATE_DATABASE.getSQLQuery());
-				statement.execute(DDLqueries.USE_DATABASE.getSQLQuery());
-				statement.execute(DDLqueries.CREATE_REGIONS_TABLE.getSQLQuery());
-				statement.execute(DDLqueries.CREATE_POSTS_TABLE.getSQLQuery());
-				statement.execute(DDLqueries.CREATE_USERS_TABLE.getSQLQuery());
-				statement.execute(DDLqueries.CREATE_POSTS_BY_USERS_TABLE.getSQLQuery());
-				statement.close();
 			} catch (SQLException | IOException exc) {
 				exc.printStackTrace();
 			}
 		}
-	}
-}
-
-enum DDLqueries {
-	CREATE_DATABASE("CREATE DATABASE IF NOT EXISTS consoleProject;"),
-	USE_DATABASE("USE consoleProject;"),
-	CREATE_REGIONS_TABLE("CREATE TABLE IF NOT EXISTS regions (id BIGINT, name VARCHAR(100) NOT NULL, PRIMARY KEY(id));"),
-	CREATE_POSTS_TABLE("CREATE TABLE IF NOT EXISTS posts (id BIGINT, content VARCHAR(300) NOT NULL, created TIMESTAMP NOT NULL, updated TIMESTAMP NULL, PRIMARY KEY(id));"),
-	CREATE_USERS_TABLE("CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, "
-			+ "region BIGINT NOT NULL, role VARCHAR(30) NOT NULL, FOREIGN KEY(region) REFERENCES regions(id));"),
-	CREATE_POSTS_BY_USERS_TABLE("CREATE TABLE IF NOT EXISTS posts_by_users(user_id BIGINT, post_id BIGINT, PRIMARY KEY(user_id, post_id), "
-			+ "FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE, FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE);");
-	
-	DDLqueries(String sql) {
-		this.sqlQuery = sql;
-	}
-	
-	private String sqlQuery;
-	public String getSQLQuery() {
-		return sqlQuery;
 	}
 }
